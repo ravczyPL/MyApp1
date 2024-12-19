5b5b460539f4b2220f22fcdbfad32af638b0c233
Rafał <rafal.lesinski@outlook.com>
2024-12-19 21:46:22 +0100

    #12 tablica pracownicy

--- database/migrations/2024_12_19_122300_create_users_table.php
+++ database/migrations/2024_12_19_122300_create_users_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('password');
-            $table->string('email')->unique();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('users')) {
+            Schema::create('users', function (Blueprint $table) {
+                $table->id();
+                $table->string('name');
+                $table->string('password');
+                $table->string('email')->unique();
+                $table->timestamps();
+            });
+        }
     }
-
     /**
      * Reverse the migrations.
      */
