f6a056907a4fc5c7da9910f0f635a2b16f56e6eb
Rafał <rafal.lesinski@outlook.com>
2024-12-19 22:15:12 +0100

    #14 db change

--- database/migrations/2024_12_19_122300_create_users_table.php
+++ database/migrations/2024_12_19_122300_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('nickname');
-            $table->md5('password');
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamps();
             });
